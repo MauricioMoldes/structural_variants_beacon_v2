8d41d0acff702db4d5787b18de1d30e8c8ad9480
mmoldes <mauricio.moldes@crg.eu>
2023-08-11 12:13:39 +0200

    initial commit

---
+++ source/sv_parser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""incomplete_submissions.py: Outputs a list of EGAS accessions and relevant information  for studies that have been published but not released."""
+
+__author__ = "Arnau Soler, Mauricio Moldes"
+__version__ = "0.1"
+__maintainer__ = "Arnau Soler, Mauricio Moldes"
+__email__ = "arnau.soler@crg.eu,mauricio.moldes@crg.eu"
+__status__ = "test"
