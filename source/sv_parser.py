d46b7a88bd420a63b7e5393c61936075c58cb423
solcos <asolercosta04@gmail.com>
2023-08-18 11:26:13 +0200

    implementation of info field methods

--- source/sv_parser.py
+++ source/sv_parser.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-"""incomplete_submissions.py: Outputs a list of EGAS accessions and relevant information  for studies that have been published but not released."""
+"""sv_parse.py: Parses the annotsv annotation of a vcf file and generates a beacon compliant json array file ."""
 
 __author__ = "Arnau Soler, Mauricio Moldes"
 __version__ = "0.1"
 __maintainer__ = "Arnau Soler, Mauricio Moldes"
-__email__ = "arnau.soler@crg.eu,mauricio.moldes@crg.eu"
+__email__ = "arnau.soler@crg.eu, mauricio.moldes@crg.eu"
 __status__ = "test"
 
+import csv
+import json
 from pysam import VariantFile
 import logging
-import json
+import sys
+import os
+import socket
 
 logger = logging.getLogger('incomplete_submissions')
 
@@ -34,15 +38,14 @@ global variant_quality_filter
 global variant_quality_qual
 global position_start
 global position_end
-global position_endInteger
-global position_startInteger
+global position_end_integer
+global position_start_integer
 global position_refseqId
 global position_assemblyId
-global caseLevelData
-global caseLevelData_zygosity
-global caseLevelData_zigosity_label
-global caseLevelData_zigosity_id
-global caseLevelData_biosampleId
+global case_level_data_zygosity
+global case_level_data_zigosity_label
+global case_level_data_zigosity_id
+global case_level_data_biosampleId
 global variation
 global variation_variantType
 global variation_alternateBases
@@ -62,23 +65,95 @@ global variantInternalId
 global identifiers
 global identifiers_genomicHGVSId
 
-""" creates variant internal id """
+""" creates hostname """
+def create_hostname():
+    info_vcf2bff_hostname = print(socket.gethostname())
+    return info_vcf2bff_hostname
 
+""" creates variant file in """
+def create_filein(filename):
+    info_vcf2bff_filein = os.path.basename(filename)
+    return info_vcf2bff_filein
 
-def create_internal_id():
-    return "internal_id"
+""" creates user name """
+def create_user():
+    info_vcf2bff_user = os.getlogin()
+    return info_vcf2bff_user
 
+""" creates host number cpu """
+def create_ncpuhost():
+    info_vcf2bff_ncpuhost = os.cpu_count()
+    return info_vcf2bff_ncpuhost
 
-def create_file_in():
-    return "vcf2bff_filein"
+""" creates bff file out """
+def create_fileout():
+    info_vcf2bff_fileout = 'genomicVariantsVcf_test.json.gz' # Needs modification depending on what we want to put
+    return info_vcf2bff_fileout
 
+""" creates path (cwd) to variants file """
+def create_cwd():
+    info_vcf2bff_cwd = '/fullpath/to/data_test/beacon_4524514test/vcf' # Needs modification depending on what we want to put
+    return info_vcf2bff_cwd
+
+""" creates variants project directory name"""
+def create_projectDir():
+    info_vcf2bff_projectDir = 'beacon_4524514test' # Needs modification depending on what we want to put
+    return info_vcf2bff_projectDir
+
+""" creates version of the beacon"""
+def create_version():
+    info_vcf2bff_version = '1.0.0' # Needs modification depending on what we want to put (come from config file from deploy?)
+    return info_vcf2bff_version
+
+""" creates datasetId (default one if none is provided?)"""
+def create_datasetId():
+    info_datasetId = 'default_beacon_1' # Needs modification depending on what we want to put
+    return info_datasetId
+
+""" creates genome"""
+def create_genome():
+    info_genome = 'hg19' # Needs to be automatised (from vcf or tsv?)
+    return info_genome
 
 """ wrapper for generating internal information """
+def internal_information(data):
+
+    filename = '../data/EEE_SV-Pop_1.ALL.sites.20181204.annotated.uniq.tsv' # File name that was processed (vcf or tsv?)
+    data['info']['vcf2bff']['hostname'] = create_hostname() # Hostname
+    data['info']['vcf2bff']['filein'] = create_filein(filename) # File in
+    data['info']['vcf2bff']['user'] = create_user() # User
+    data['info']['vcf2bff']['ncpuhost'] = create_ncpuhost() # Number of cpu's
+    data['info']['vcf2bff']['fileout'] = create_fileout() # File out
+    data['info']['vcf2bff']['cwd'] = create_cwd() # Path to output folder
+    data['info']['vcf2bff']['cwd'] = create_cwd() # Path to output folder
+    data['info']['vcf2bff']['projectDir'] = create_projectDir() # Path to output final project directory
+    data['info']['vcf2bff']['version'] = create_version() # Version of the Beacon
 
 
-def internal_information():
-    create_internal_id()
-    create_file_in()
+""" Receives Genotype, returns zygosity"""
+
+def case_level_data(genotype):
+    zygosity = None
+
+    if genotype == '0/1' or genotype == '0|1' or genotype == '1/0' or genotype == '1|0' or genotype == '0':
+        zygosity = "GENO:GENO_0000458"
+    if genotype == '1/1' or genotype == '1|1' or genotype == '1':
+        zygosity = "GENO:GENO_0000136"
+
+    return zygosity
+
+
+""" Verifies if multiple samples are present, assign each genotype to sample"""
+
+
+def multi_sample_case_level_data(case_level_data_biosampleId, sample_genotypes):
+    samples = []
+    genotypes = []
+    if "," in case_level_data_biosampleId:  # uses sample field to verify if multi sample
+        samples = case_level_data_biosampleId.split(sep=",")  # split sample
+
+        #for samples, sample_genotypes in:
+            #return None  # assign genotype and zygosity per sample
 
 
 """ reads vcf """
@@ -92,15 +167,100 @@ def read_vcf():
         bcf_out.write(rec)
 
 
-def read_tsv():
-    TODO
+def pre_process_tsv():
+    os.system("cat ../data/lumpy.annotated.tsv | cut -f1-15 | sort | uniq > ../data/lumpy.annotated.uniq.tsv")
+
+
+def read_tsv(data):
+    with open('../data/EEE_SV-Pop_1.ALL.sites.20181204.annotated.uniq.tsv') as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        for row in rd:
+            position_start_integer = row[2]
+            position_end_integer = row[3]
+            position_start = row[2]
+            position_end = row[3]
+            position_refseqId = row[1]
+            position_assemblyId = "hg19"
+            case_level_data_zigosity_label_raw = row[14]
+            case_level_data_zigosity_label = case_level_data_zigosity_label_raw.split(sep=":")[0]
+            list_genotypes = case_level_data_zigosity_label_raw.split(sep=":")
+            case_level_data_zigosity_id = case_level_data(case_level_data_zigosity_label)
+            case_level_data_biosampleId = row[6]
+
+            multi_sample_case_level_data(case_level_data_biosampleId, list_genotypes)
+
+            variation_variant_type = row[5]
+            variation_alternate_bases = row[9]
+            variation_reference_bases = row[8]
+            variation_location_interval_start_value = row[2]
+            variation_location_interval_start_type = "Number"
+            variation_location_interval_end_value = row[3]
+            variation_location_interval_end_type = "Number"
+            variation_location_type = "SequenceLocation"
+            variation_location_sequence_id = row[5]
+            variant_internal_id = row[0]
+            identifiers_genomic_hgvs_id = row[5]
+            variant_quality_filter = row[11]
+            variant_quality_qual = row[10]
+            variation_location_interval_type = "TODO"
+
+            data['variantInternalId'] = variant_internal_id
+            data['identifiers']['genomicHGVSId'] = identifiers_genomic_hgvs_id
+            data['_position']['assemblyId'] = position_assemblyId
+            data['_position']['end'] = position_end
+            data['_position']['endInteger'] = position_end_integer
+            data['_position']['refseqId'] = position_refseqId
+            data['_position']['startInteger'] = position_start_integer
+            data['_position']['start'] = position_start
+
+            data['variantQuality']['QUAL'] = variant_quality_qual
+            data['variantQuality']['FILTER'] = variant_quality_filter
+
+            data['variation']['location']['sequence_id'] = variation_location_sequence_id
+            data['variation']['location']['type'] = variation_location_type
+            data['variation']['location']['interval']['start']['type'] = variation_location_interval_start_type
+            data['variation']['location']['interval']['start']['value'] = variation_location_interval_start_value
+            data['variation']['location']['interval']['end']['type'] = variation_location_interval_end_type
+            data['variation']['location']['interval']['end']['value'] = variation_location_interval_end_value
+            data['variation']['location']['interval']['type'] = variation_location_interval_type
+
+            data['variation']['alternateBases'] = variation_alternate_bases
+            data['variation']['referenceBases'] = variation_reference_bases
+            data['variation']['variantType'] = variation_variant_type
+
+            # TODO
+            # data['caseLevelData']['zygosity']['label'] = case_level_data_zigosity_label
+            # data['caseLevelData']['zygosity']['id'] = case_level_data_zigosity_id
+            # data['caseLevelData']['biosampleId'] = case_level_data_biosampleId
+
+            write_json(data)
+
+
+def bff_post_processing():
+    os.system("sed -i 's/}{/},{/' ../results/g_variants_sv.json")
 
 
 """ write bff output in json array format """
 
 
-def write_json():
-    TODO
+def read_json():
+    ## Transform from bff to python dictionary
+    with open('../data/first_json_template.json') as json_file:
+        data = json.load(json_file)  # Load json to python
+
+    return data
+
+
+""" write bff output in json array format """
+
+
+def write_json(data):
+    # Serializing json
+    json_object = json.dumps(data, indent=4)
+
+    # Writing to sample.json
+    with open("../results/g_variants_sv.json", "a") as outfile:
+        outfile.write(json_object)
 
 
 if __name__ == "__main__":
@@ -109,7 +269,12 @@ if __name__ == "__main__":
         log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s [in %(pathname)s:%(lineno)d]'
         logging.basicConfig(format=log_format)
         # execute main function
-        read_vcf()
+        data = read_json()  # READS BFF TEMPLATE
+        # pre_process_tsv()
+        read_tsv(data)  # POPULATE STUFF
+        internal_information(data)  # POPULATE INTERNAL INFORMATION
+        bff_post_processing()
+
     except Exception as e:
         logger.error("Error: {}".format(e))
         sys.exit(-1)
